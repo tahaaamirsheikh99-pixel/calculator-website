4a20ab5be0f326cebc1846f0cd9bccb613ff219a
tahaaamirsheikh99-pixel <tahaaamirsheikh99@gmail.com>
2026-03-04 14:10:15 +0500

    cal.js

--- cal.js
+++ cal.js
@@ -1,48 +1,51 @@
 const inputBox = document.getElementById('inputBox');
 const buttons = document.querySelectorAll('button');
 
-let currentInput = "";
+let string = "";
 
 buttons.forEach(button => {
   button.addEventListener('click', (e) => {
-    const value = e.target.innerText;
+    let btnText = e.target.innerText;
 
     try {
-      if (value === '=') {
-        currentInput = eval(currentInput).toString();
-        inputBox.value = currentInput;
+      if (btnText === '=') {
+        string = eval(string).toString();
+        inputBox.value = string;
       } 
-      else if (value === 'AC') {
-        currentInput = "";
+      else if (btnText === 'AC') {
+        string = "";
         inputBox.value = "0";
       } 
-      else if (value === 'DEL') {
-        currentInput = currentInput.slice(0, -1);
-        inputBox.value = currentInput || "0";
+      else if (btnText === 'DEL') {
+        string = string.slice(0, -1);
+        inputBox.value = string || "0";
       } 
-      else if (value === '++') {
-        currentInput = (parseFloat(eval(currentInput || "0")) + 1).toString();
-        inputBox.value = currentInput;
+      else if (btnText === '++') {
+           string = (parseFloat(eval(string || "0")) + 1).toString();
+        inputBox.value = string;
       } 
-      else if (value === '--') {
-        currentInput = (parseFloat(eval(currentInput || "0")) - 1).toString();
-        inputBox.value = currentInput;
+      else if (btnText === '--') {
+    
+        string = (parseFloat(eval(string || "0")) - 1).toString();
+        inputBox.value = string;
       }
       else if (e.target.id === 'plusminus') {
-         currentInput = (parseFloat(eval(currentInput || "0")) * -1).toString();
-        inputBox.value = currentInput;
+        
+        string = (parseFloat(eval(string || "0")) * -1).toString();
+        inputBox.value = string;
       }
       else {
-         if (inputBox.value === "0" && !isNaN(value)) {
-          currentInput = value;
+   
+        if (inputBox.value === "0" && btnText !== ".") {
+          string = btnText;
         } else {
-          currentInput += value;
+          string += btnText;
         }
-        inputBox.value = currentInput;
+        inputBox.value = string;
       }
-    } catch (error) {
+    } catch (err) {
       inputBox.value = "Error";
-      currentInput = "";
+      string = "";
     }
   });
 });
